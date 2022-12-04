452ada276e66e511ccbe112be83d1c84dba86a3e
kling111 <kling111@gmail.com>
2022-12-04 14:20:48 -0600

    bug fix

--- web_contents_bucket.tf
+++ web_contents_bucket.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket_versioning" "web_contents_bucket_versioning" {
 
 resource "aws_s3_bucket_object" "web_contents_html" {
   bucket = aws_s3_bucket.web_contents_bucket.id
-  key    = "profile"
-  acl    = "web_contents.html"
+  key    = "web_contents.html"
+  acl    = "private"
   source = "web_contents.html"
 }
