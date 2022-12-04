680db488e321097174bf9e1d32855d7d3d19658a
kling111 <kling111@gmail.com>
2022-12-04 13:41:50 -0600

    added new html

--- ec2_webhost.tf
+++ ec2_webhost.tf
@@ -1,42 +1,19 @@
-resource "aws_security_group" "webhost_security_group" {
-    name = "webhost_security_group"
-    description = "Allows port connections for webhosting"
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
 resource "aws_instance" "ec2_webhost_apache" {
     ami = var.webhost_ec2_ami
     instance_type = var.webhost_ec2_type
     security_groups = ["${aws_security_group.webhost_security_group.name}"]
     key_name = "webhost_kp"
 
-    user_data = <<-EOF
-        #! /bin/bash
-        sudo yum install httpd -y
-        sudo systemctl start httpd
-        sudo systemctl enable httpd
-        echo "Hello World, this is Karan Lingineni" >> /var/www/html/index.html
-    EOF
+    provisioner "file" {
+        source      = "web_contents.html"
+        destination = "/var/www/html/index.html"
+
+        connection {   
+        host        = self.public_ip
+        user        = "root"
+        private_key = file("webhost_kp.pem")
+        }   
+    }
 
     tags = {
       Name = "ec2_webhost_apache"
---
+++ security_groups.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "webhost_security_group" {
+    name = "webhost_security_group"
+    description = "Allows port connections for webhosting"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
