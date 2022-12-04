38c55f1dc40202a7c288a1431895d1b5a3270b45
kling111 <kling111@gmail.com>
2022-12-04 14:18:37 -0600

    added acl/versioning

--- web_contents_bucket.tf
+++ web_contents_bucket.tf
@@ -1,6 +1,17 @@
 resource "aws_s3_bucket" "web_contents_bucket" {
   bucket = "web_contents_bucket"
+}
+
+resource "aws_s3_bucket_acl" "web_contents_bucket_acl" {
+  bucket = aws_s3_bucket.web_contents_bucket.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "web_contents_bucket_versioning" {
+  bucket = aws_s3_bucket.web_contents_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_object" "web_contents_html" {
