3e70be65c52ed8c7ad818bd124b0502295a01bdf
kling111 <kling111@gmail.com>
2022-12-05 16:42:16 -0600

    Added security group

--- ec2_webhost.tf
+++ ec2_webhost.tf
@@ -50,10 +50,36 @@ resource "aws_iam_instance_profile" "webhost_instance_profile" {
   
 }
 
+resource "aws_security_group" "webhost_security_group" {
+    name = "webhost_security_group"
+    description = "Allows port connections for webhosting"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 resource "aws_instance" "project_salvera_ec2_webhost" {
     ami = var.webhost_ec2_ami
     instance_type = var.webhost_ec2_type
-    security_groups = ["webhost_security_group"]
+    security_groups = ["${aws_security_group.webhost_security_group.name}"]
     iam_instance_profile = aws_iam_instance_profile.webhost_instance_profile.name
     key_name = "webhost_kp"
 
