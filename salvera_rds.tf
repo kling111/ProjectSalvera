0e4cf48f9def1d4784ad13e37696d10be92d597c
kling111 <kling111@gmail.com>
2022-12-07 01:31:28 -0600

    Added lambda role

--- salvera_rds.tf
+++ salvera_rds.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "salvera_admins" {
-  allocated_storage    = 2
+  allocated_storage    = 1
   db_name              = "salvera_admins"
   engine               = "mysql"
   instance_class       = "db.t2.micro"
