9f4570735007653c3a2a241b6366a4789cf9fb13
kling111 <kling111@gmail.com>
2022-12-08 19:55:17 -0800

    added pymysql

--- salvera_lambdas.tf
+++ salvera_lambdas.tf
@@ -1,23 +1,3 @@
-resource "aws_lambda_function" "submit_form" {
-  function_name = "SubmitForm"
-
-  s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_object.lambdas_upload.key
-
-  runtime = "python3.9"
-  handler = "submit_form.handler"
-
-  source_code_hash = data.archive_file.lambdas_zip.output_base64sha256
-
-  role = aws_iam_role.salvera_lambda_role.arn
-}
-
-resource "aws_cloudwatch_log_group" "submit_form_logs" {
-  name = "/aws/lambda/${aws_lambda_function.submit_form.function_name}"
-
-  retention_in_days = 30
-}
-
 resource "aws_iam_role" "salvera_lambda_role" {
   name = "salvera_lambda"
 
@@ -38,4 +18,24 @@ resource "aws_iam_role" "salvera_lambda_role" {
 resource "aws_iam_role_policy_attachment" "salvera_lambda_policy" {
   role       = aws_iam_role.salvera_lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_lambda_function" "submit_form" {
+  function_name = "SubmitForm"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambdas_upload.key
+
+  runtime = "python3.9"
+  handler = "submit_form.handler"
+
+  source_code_hash = data.archive_file.lambdas_zip.output_base64sha256
+
+  role = aws_iam_role.salvera_lambda_role.arn
+}
+
+resource "aws_cloudwatch_log_group" "submit_form_logs" {
+  name = "/aws/lambda/${aws_lambda_function.submit_form.function_name}"
+
+  retention_in_days = 30
 }
