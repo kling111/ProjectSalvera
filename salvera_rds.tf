7c9f20c1e74784ea8f3595a1cf62a9ee0bc88d01
kling111 <kling111@gmail.com>
2022-12-08 18:16:19 -0800

    Added db security group

--- salvera_rds.tf
+++ salvera_rds.tf
@@ -1,7 +1,27 @@
+resource "aws_security_group" "mysql_security_group" {
+    name = "mysql_security_group"
+    description = "Allows port connections for MySQL database"
+
+    ingress {
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 resource "aws_rds_cluster" "salvera_aurora_cluster" {
   cluster_identifier = "salvera-aurora-cluster"
   master_username = jsondecode(data.aws_secretsmanager_secret_version.salvera_aurora_creds.secret_string)["master_username"]
   master_password = jsondecode(data.aws_secretsmanager_secret_version.salvera_aurora_creds.secret_string)["master_password"]
+  vpc_security_group_ids = ["${aws_security_group.mysql_security_group.id}"]
 }
 
 resource "aws_rds_cluster_instance" "salvera_aurora_instance" {
