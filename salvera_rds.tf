dec433574e2361cb1f704535705d2d9227747519
kling111 <kling111@gmail.com>
2022-12-08 16:17:31 -0800

    changed cluster name

--- salvera_rds.tf
+++ salvera_rds.tf
@@ -1,5 +1,5 @@
 resource "aws_rds_cluster" "salvera_aurora_cluster" {
-  cluster_identifier      = "salvera_db_cluster"
+  cluster_identifier      = "salvera-aurora-cluster"
   master_username         = jsondecode(data.aws_secretsmanager_secret_version.salvera_aurora_creds.secret_string)["master_username"]
   master_password         = jsondecode(data.aws_secretsmanager_secret_version.salvera_aurora_creds.secret_string)["master_password"]
 }
