a5a82f351efb7c94d29fb5c5337a3fdad6995749
kling111 <kling111@gmail.com>
2022-12-14 18:02:43 -0800

    changed hash key

--- salvera_ddb.tf
+++ salvera_ddb.tf
@@ -1,6 +1,6 @@
 resource "aws_dynamodb_table" "salvera_patient_data" {
   name     = "salvera_patient_data"
-  hash_key = "patient_id"
+  hash_key = "patient_uuid"
 
   attribute {
     name = "patient_uuid"
