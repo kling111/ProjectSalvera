142e49b9f8f7336aa2f792b352a335ec4890dc00
kling111 <kling111@gmail.com>
2022-12-14 18:06:42 -0800

    changed billing mode for ddb

--- salvera_ddb.tf
+++ salvera_ddb.tf
@@ -1,6 +1,7 @@
 resource "aws_dynamodb_table" "salvera_patient_data" {
-  name     = "salvera_patient_data"
-  hash_key = "patient_uuid"
+  name         = "salvera_patient_data"
+  hash_key     = "patient_uuid"
+  billing_mode = "PAY_PER_REQUEST"
 
   attribute {
     name = "patient_uuid"
